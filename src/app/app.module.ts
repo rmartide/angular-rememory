17e9975b4bf2a20905a0bb93679ee84a11aec187
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2018-11-06 14:38:24 +0100

    Added empty routes and router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
+import { RouterModule } from '@angular/router';
+import appRoutes from '@app/routes/routes';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+];
+
+
+export default appRoutes;
