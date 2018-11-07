2761349b6703325d0af38c4325e686d75b99509b
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2018-11-07 12:49:37 +0100

    Added back button that gets hidden on /boxes with a directive

---
+++ src/app/directives/hideonroute.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HideonrouteDirective } from './hideonroute.directive';
+
+describe('HideonrouteDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HideonrouteDirective();
+    expect(directive).toBeTruthy();
+  });
+});
