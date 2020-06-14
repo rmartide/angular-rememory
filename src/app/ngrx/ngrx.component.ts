5d11843e127518c69685399e9fc45472d5977025
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-06-14 20:09:08 +0200

    ngrx component

---
+++ src/app/ngrx/ngrx.component.ts
@@ -0,0 +1,24 @@
+import { State } from './../store/state';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { YoutubeItem } from '@app/rxjs/youtube-search';
+
+@Component({
+  selector: 'app-ngrx',
+  templateUrl: './ngrx.component.html',
+  styleUrls: ['./ngrx.component.scss']
+})
+export class NgrxComponent implements OnInit {
+
+  searchItems: YoutubeItem[] = [];
+
+  constructor(private store: Store<{youtubeSearch: State}>) { }
+
+  ngOnInit(): void {
+    this.store.subscribe(({youtubeSearch}) => {
+      console.log(youtubeSearch)
+      this.searchItems = youtubeSearch?.searchItems;
+    })
+  }
+
+}
