1bd5f5fde63c78a553f5c5595045080d2df92b4b
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-08-07 21:08:36 +0200

    Adding baseHref and changing the directory for the build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'remember-app'`, () => {
+  it(`should have as title 'angular-rememory'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('remember-app');
+    expect(app.title).toEqual('angular-rememory');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to remember-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-rememory!');
   });
 });
